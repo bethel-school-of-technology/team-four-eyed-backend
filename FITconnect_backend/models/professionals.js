282a7424ccdac0bc19629b6c74300b8f36c046dd
dmcmichae1 <mcmichaeldevin@icloud.com>
2021-07-19 20:01:41 -0700

    sequelize, nodemon, NPM, SQL, database-DM

---
+++ FITconnect_backend/models/professionals.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Professionals extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Professionals.init({
+    firstName: DataTypes.STRING,
+    allowNull: false,
+
+    lastName: DataTypes.STRING,
+    allowNull: false,
+
+    username: DataTypes.STRING,
+    allowNull: false,
+
+    email: DataTypes.STRING,
+    allowNull: false,
+
+    password: DataTypes.STRING,
+    allowNull: false
+  }, {
+    sequelize,
+    modelName: 'Professionals',
+  });
+  return Professionals;
+};
