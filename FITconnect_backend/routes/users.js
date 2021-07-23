ddf323ba88597289b08c69d7305afdd0f5ffe8e2
GabrielCabrera4950 <cabreragapc@gmail.com>
2021-07-22 18:59:04 -0700

    started jwt auth and created a user.js in models

--- FITconnect_backend/routes/users.js
+++ FITconnect_backend/routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
+const { User } = require('..models');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// POST User
+router.post('/', (req, res, next) => {
+
+  if (req.body.username || !req.body.password) {
+    res.status(400).send('Username and pw required');
+    return;
+  }
+
+
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(newUser => {
+    res.json({
+      id: newUser.id,
+      username: newUser.username
+    });
+
+  }).catch(() => {
+    res.status(400).send();
+  });
 });
+
+
 
 module.exports = router;
